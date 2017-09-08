7a62d232d5498bf084b6108c89eb2c0b6bd0c39b
unknown <nnk2402@gmail.com>
2017-09-08 22:51:20 +0700

    [backend] logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,8 @@
 
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{--{{ config('app.name', 'Laravel') }}--}}
+                    Story
                 </a>
             </div>
 
