0d10216d7bcfec682eceaa76a42507f15da17741
unknown <nnk2402@gmail.com>
2017-09-29 10:38:00 +0700

    [frontend] css

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -24,7 +24,7 @@
 
 
                     <div class="container">
-                        <hr style="background: #DEAAC8">
+                        <hr class="end-story">
                     </div>
                 @endforeach
 
