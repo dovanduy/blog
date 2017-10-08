df155d67922678882a559cd50f61e353df9ee84d
unknown <nnk2402@gmail.com>
2017-10-08 10:01:10 +0700

    ~~~

--- app/Http/Controllers/Backend/ToolController.php
+++ app/Http/Controllers/Backend/ToolController.php
@@ -56,7 +56,7 @@ class ToolController extends Controller
         $start_content = strpos($html, $start) + strlen($start);
         $end_content = strpos($html, $end) - $start_content;
         $result = substr($html, $start_content, $end_content);
-        if( strlen($result ) <= 255) {
+        if (strlen($result) <= 255) {
             return $result;
         } else {
             return '';
@@ -65,60 +65,24 @@ class ToolController extends Controller
 
     //
 
-    public function get_remote_data($url, $post_paramtrs = false) {
-        $c = curl_init();
-        curl_setopt($c, CURLOPT_URL, $url);
-        curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-        if ($post_paramtrs) {
-            curl_setopt($c, CURLOPT_POST, TRUE);
-            curl_setopt($c, CURLOPT_POSTFIELDS, "var1=bla&" . $post_paramtrs);
-        } curl_setopt($c, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($c, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; rv:33.0) Gecko/20100101 Firefox/33.0");
-        curl_setopt($c, CURLOPT_COOKIE, 'CookieName1=Value;');
-        curl_setopt($c, CURLOPT_MAXREDIRS, 10);
-        $follow_allowed = ( ini_get('open_basedir') || ini_get('safe_mode')) ? false : true;
-        if ($follow_allowed) {
-            curl_setopt($c, CURLOPT_FOLLOWLOCATION, 1);
-        }curl_setopt($c, CURLOPT_CONNECTTIMEOUT, 9);
-        curl_setopt($c, CURLOPT_REFERER, $url);
-        curl_setopt($c, CURLOPT_TIMEOUT, 60);
-        curl_setopt($c, CURLOPT_AUTOREFERER, true);
-        curl_setopt($c, CURLOPT_ENCODING, 'gzip,deflate');
-        $data = curl_exec($c);
-        $status = curl_getinfo($c);
-        curl_close($c);
-        preg_match('/(http(|s)):\/\/(.*?)\/(.*\/|)/si', $status['url'], $link);
-        $data = preg_replace('/(src|href|action)=(\'|\")((?!(http|https|javascript:|\/\/|\/)).*?)(\'|\")/si', '$1=$2' . $link[0] . '$3$4$5', $data);
-        $data = preg_replace('/(src|href|action)=(\'|\")((?!(http|https|javascript:|\/\/)).*?)(\'|\")/si', '$1=$2' . $link[1] . '://' . $link[3] . '$3$4$5', $data);
-        if ($status['http_code'] == 200) {
-            return $data;
-        } elseif ($status['http_code'] == 301 || $status['http_code'] == 302) {
-            if (!$follow_allowed) {
-                if (empty($redirURL)) {
-                    if (!empty($status['redirect_url'])) {
-                        $redirURL = $status['redirect_url'];
-                    }
-                } if (empty($redirURL)) {
-                    preg_match('/(Location:|URI:)(.*?)(\r|\n)/si', $data, $m);
-                    if (!empty($m[2])) {
-                        $redirURL = $m[2];
-                    }
-                } if (empty($redirURL)) {
-                    preg_match('/href\=\"(.*?)\"(.*?)here\<\/a\>/si', $data, $m);
-                    if (!empty($m[1])) {
-                        $redirURL = $m[1];
-                    }
-                } if (!empty($redirURL)) {
-                    $t = debug_backtrace();
-                    return call_user_func($t[0]["function"], trim($redirURL), $post_paramtrs);
-                }
-            }
-        } return "ERRORCODE22 with $url!!<br/>Last status codes<b/>:" . json_encode($status) . "<br/><br/>Last data got<br/>:$data";
+    public function file_get_contents_curl($url)
+    {
+        header("Content-Type: text/html; charset=utf-8");
+        $ch=curl_init();
+        curl_setopt($ch,CURLOPT_HEADER,0);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($ch,CURLOPT_URL,$url);
+        curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1);
+        $data=curl_exec($ch);
+        curl_close($ch);
+        return $data;
     }
 
     public function getStory(Request $request)
     {
+
+        return get_web_page('http://truyensex88.net/con-nho-bien-thai-p2-bao-dam-manh.html');
+        return $this->getCookies('http://truyensex88.net/con-nho-bien-thai-p2-bao-dam-manh.html');
     }
 
 
