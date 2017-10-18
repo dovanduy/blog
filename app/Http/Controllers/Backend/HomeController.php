3193c35a66c31ed4a957d055db0eb89fbe5a9056
unknown <nnk2402@gmail.com>
2017-10-18 11:56:29 +0700

    view

--- app/Http/Controllers/Backend/HomeController.php
+++ app/Http/Controllers/Backend/HomeController.php
@@ -18,6 +18,149 @@ class HomeController extends Controller
 
     public function index()
     {
+        $post_id = json_decode(Post::where('user_id', Auth::id())->pluck('id'));
+        $post_views_id = json_decode(View::pluck('post_id'));
+
+        foreach ($post_views_id as $val) {
+            $count_view_story_id = View::wherePost_id($val)->first();
+            if (count($count_view_story_id) != 0) {
+                $time_db_now = date('Y-m-d', strtotime($count_view_story_id->updated_at));
+                $time_now = date('Y-m-d', time());
+                if ($time_db_now != $time_now) {
+                    $time_old = strtotime($time_now) - strtotime($time_db_now);
+                    $day = $time_old / (3600 * 24);
+
+                    switch ($day) {
+                        case 1:
+                            $day_2 = $count_view_story_id->today;
+                            $day_3 = $count_view_story_id->day_2;
+                            $day_4 = $count_view_story_id->day_3;
+                            $day_5 = $count_view_story_id->day_4;
+                            $day_6 = $count_view_story_id->day_5;
+                            $day_7 = $count_view_story_id->day_6;
+                            $day_8 = $count_view_story_id->day_7;
+
+                            $count_view_story_id->today = 0;
+                            $count_view_story_id->day_2 = $day_2;
+                            $count_view_story_id->day_3 = $day_3;
+                            $count_view_story_id->day_4 = $day_4;
+                            $count_view_story_id->day_5 = $day_5;
+                            $count_view_story_id->day_6 = $day_6;
+                            $count_view_story_id->day_7 = $day_7;
+                            $count_view_story_id->day_8 = $day_8;
+                            $count_view_story_id->save();
+                            break;
+                        case 2:
+                            $day_3 = $count_view_story_id->today;
+                            $day_4 = $count_view_story_id->day_2;
+                            $day_5 = $count_view_story_id->day_3;
+                            $day_6 = $count_view_story_id->day_4;
+                            $day_7 = $count_view_story_id->day_5;
+                            $day_8 = $count_view_story_id->day_6;
+
+                            $count_view_story_id->today = 0;
+                            $count_view_story_id->day_2 = 0;
+                            $count_view_story_id->day_3 = $day_3;
+                            $count_view_story_id->day_4 = $day_4;
+                            $count_view_story_id->day_5 = $day_5;
+                            $count_view_story_id->day_6 = $day_6;
+                            $count_view_story_id->day_7 = $day_7;
+                            $count_view_story_id->day_8 = $day_8;
+                            $count_view_story_id->save();
+
+                            break;
+                        case 3:
+                            $day_4 = $count_view_story_id->today;
+                            $day_5 = $count_view_story_id->day_2;
+                            $day_6 = $count_view_story_id->day_3;
+                            $day_7 = $count_view_story_id->day_4;
+                            $day_8 = $count_view_story_id->day_5;
+
+                            $count_view_story_id->today = 0;
+                            $count_view_story_id->day_2 = 0;
+                            $count_view_story_id->day_3 = 0;
+                            $count_view_story_id->day_4 = $day_4;
+                            $count_view_story_id->day_5 = $day_5;
+                            $count_view_story_id->day_6 = $day_6;
+                            $count_view_story_id->day_7 = $day_7;
+                            $count_view_story_id->day_8 = $day_8;
+                            $count_view_story_id->save();
+
+                            break;
+                        case 4:
+                            $day_5 = $count_view_story_id->today;
+                            $day_6 = $count_view_story_id->day_2;
+                            $day_7 = $count_view_story_id->day_3;
+                            $day_8 = $count_view_story_id->day_4;
+
+                            $count_view_story_id->today = 0;
+                            $count_view_story_id->day_2 = 0;
+                            $count_view_story_id->day_3 = 0;
+                            $count_view_story_id->day_4 = 0;
+                            $count_view_story_id->day_5 = $day_5;
+                            $count_view_story_id->day_6 = $day_6;
+                            $count_view_story_id->day_7 = $day_7;
+                            $count_view_story_id->day_8 = $day_8;
+                            $count_view_story_id->save();
+
+                            break;
+                        case 5:
+                            $day_6 = $count_view_story_id->today;
+                            $day_7 = $count_view_story_id->day_2;
+                            $day_8 = $count_view_story_id->day_3;
+
+                            $count_view_story_id->today = 0;
+                            $count_view_story_id->day_2 = 0;
+                            $count_view_story_id->day_3 = 0;
+                            $count_view_story_id->day_4 = 0;
+                            $count_view_story_id->day_5 = 0;
+                            $count_view_story_id->day_6 = $day_6;
+                            $count_view_story_id->day_7 = $day_7;
+                            $count_view_story_id->day_8 = $day_8;
+                            $count_view_story_id->save();
+
+                            break;
+                        case 6:
+                            $day_7 = $count_view_story_id->today;
+                            $day_8 = $count_view_story_id->day_2;
+
+                            $count_view_story_id->today = 0;
+                            $count_view_story_id->day_2 = 0;
+                            $count_view_story_id->day_3 = 0;
+                            $count_view_story_id->day_4 = 0;
+                            $count_view_story_id->day_5 = 0;
+                            $count_view_story_id->day_6 = 0;
+                            $count_view_story_id->day_7 = $day_7;
+                            $count_view_story_id->day_8 = $day_8;
+                            $count_view_story_id->save();
+                            break;
+                        case 7:
+                            $day_8 = $count_view_story_id->today;
+
+                            $count_view_story_id->today = 0;
+                            $count_view_story_id->day_2 = 0;
+                            $count_view_story_id->day_3 = 0;
+                            $count_view_story_id->day_4 = 0;
+                            $count_view_story_id->day_5 = 0;
+                            $count_view_story_id->day_6 = 0;
+                            $count_view_story_id->day_7 = 0;
+                            $count_view_story_id->day_8 = $day_8;
+                            $count_view_story_id->save();
+                            break;
+                        default:
+                            $count_view_story_id->today = 0;
+                            $count_view_story_id->day_2 = 0;
+                            $count_view_story_id->day_3 = 0;
+                            $count_view_story_id->day_4 = 0;
+                            $count_view_story_id->day_5 = 0;
+                            $count_view_story_id->day_6 = 0;
+                            $count_view_story_id->day_7 = 0;
+                            $count_view_story_id->day_8 = 0;
+                            $count_view_story_id->save();
+                    }
+                }
+            }
+        }
 
         $chart = [
             'days' => [],
@@ -40,8 +183,6 @@ class HomeController extends Controller
         $day_7 = View::sum('day_7');
         $day_8 = View::sum('day_8');
         $chart['views'] = array($day_8, $day_7, $day_6, $day_5, $day_4, $day_3, $day_2, $today);
-
-        $post_id = json_decode(Post::where('user_id', Auth::id())->pluck('id'));
 
         $today = View::whereIn('post_id', $post_id)->sum('today');
         $day_2 = View::whereIn('post_id', $post_id)->sum('day_2');
